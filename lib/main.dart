221c574c0bd077e68bbb044ebf2ee7672cfe9442
Razaul Munir <razaulmunir@gmail.com>
2021-03-28 09:08:19 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the the
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Flutter App Bar"),),
+        body: Center(
+          child: Text("I am in center"),
+        ),
+      ),
+    );
+  }
+}
